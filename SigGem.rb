d0e706f92f3a84abdd73d766237c7f0bc5213c6d
Andy <g_andy@live.com>
2025-01-03 17:17:49 -0800

    Update SigGem.rb

--- SigGem.rb
+++ SigGem.rb
@@ -3,7 +3,7 @@
 #Written By Andy P. (Icy_Viking)  #
 #Icy Viking Games 	          #
 #Ruby wrapper for sigil           #
-#Copyright (c) 2024               #
+#Copyright (c) 2025              #
 ################################### 
 require 'ffi'
 
